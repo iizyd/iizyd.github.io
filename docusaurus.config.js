51642797445c489e116a7277cfccd5de6c49bd9a
zhiyudong <yudong.zhi@chaitin.com>
2023-10-17 17:42:10 +0800

    feat: update config;

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "iizyd", // Usually your GitHub org/user name.
-  projectName: "my-website", // Usually your repo name.
+  projectName: "iizyd.github.io", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
